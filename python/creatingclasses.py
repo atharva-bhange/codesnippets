370ec3bf4b165800709c7b3d6e40a742c242352a
atharva-bhange <atharva.bhange@gmail.com>
2020-02-11 01:00:33 +0530

    changes

--- python/creatingclasses.py
+++ python/creatingclasses.py
@@ -161,3 +161,25 @@ jim = cat("jim", 14, "black")
 mark.speak()
 jim.speak()
 '''
+
+#Snippet 9
+class point():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def __add__(self, p):
+        return point(self.x + p.x, self.y +p.y)
+    def __sub__(self, p):
+        return point(self.x - p.x , self.y - p.y)
+    def __str__(self):
+        return "("+str(self.x)+","+str(self.y)+")"
+
+p1 = point(2,3)
+p2 = point(4,5)
+p3 = point(-2,3)
+p4 = point(-3,-8)
+
+p5 = p1 + p2
+p6 = p1 - p2
+print(p5)
+print(p6)
